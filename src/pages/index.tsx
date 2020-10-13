97ea3161fbcf8f25ef8835aa3f8ae02870fb72cb
lanhao <admin@lanhao.name>
2020-10-13 13:54:55 +0800

    feat: 手动断句

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,45 @@
-import { Input } from 'antd';
+import { Col, Input, Row } from 'antd';
 import React, { useState } from 'react';
 
 import styles from './index.less';
 
-function genTitle(str: string) {
-  let [head, tail] = [str.substr(0, str.length - 1), str.substr(-1)]
-  if (str.length > 4) {
-    [head, tail] = [str.substr(0, str.length - 2), str.substr(-2)]
-  }
+function genTitle(head: string, tail: string) {
+
   return (
-    <div style={{ backgroundColor: "black",fontSize:"36px",fontWeight:"bold",padding:"10px 1rem" }}>
-      <span style={{color:"white"}}>{head}</span>
-      <span style={{color:"black",backgroundColor:"orange",padding:"2px 5px",borderRadius:"3px"}}>{tail}</span>
+    <div style={{ backgroundColor: "black", fontSize: "36px", fontWeight: "bold", padding: "10px 1rem" }}>
+      <span style={{ color: "white" }}>{head}</span>
+      <span style={{ color: "black", backgroundColor: "orange", padding: "2px 5px", borderRadius: "3px" }}>{tail}</span>
     </div>
   );
 }
 
 export default () => {
-  const [text, setText] = useState('正人君子');
+  const [head, setHead] = useState('正人');
+  const [tail, setTail] = useState('君子');
   return (
     <div className={styles.block}>
-      <Input
-        type="text"
-        defaultValue={text}
-        size="large"
-        onChange={e => setText(e.target.value)}
-      />
-      <p>
-        {genTitle(text)}
+      <Input.Group size="large">
+        <Row gutter={8}>
+          <Col span={5}>
+            <Input
+              type="text"
+              defaultValue={head}
+              size="large"
+              onChange={e => setHead(e.target.value)}
+            />
+          </Col>
+          <Col span={5}>
+            <Input
+              type="text"
+              defaultValue={tail}
+              size="large"
+              onChange={e => setTail(e.target.value)}
+            />
+          </Col>
+        </Row>
+      </Input.Group>
+      <p style={{marginTop:"10px"}}>
+        {genTitle(head, tail)}
       </p>
     </div>
   );
